cb0b376e6e658380c7440009ddb5c3c546bf9883
Vlada Gherasim <vladagherasim@gmail.com>
2022-05-27 17:35:21 +0300

    Added product entity

---
+++ app/src/main/java/com/example/wowcart/ui/Product.kt
@@ -0,0 +1,9 @@
+package com.example.wowcart.ui
+
+data class Product(
+val image: String,
+val title: String,
+val description: String,
+val price: Double,
+val isFavorite: Boolean
+)
---
+++ app/src/main/java/com/example/wowcart/ui/ProductAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.wowcart.ui
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductAdapter {
+    class ViewHolder(itemView: View) : View.OnClickListener, RecyclerView.ViewHolder(itemView) {
+
+        override fun onClick(v: View?) {
+            TODO("Not yet implemented")
+        }
+
+    }
+}
