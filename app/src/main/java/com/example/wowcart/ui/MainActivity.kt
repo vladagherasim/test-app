61326c483e46fb42f78b67b75754b11b4525a41e
alexei.birna <alexei.birna@ebs-integrator.com>
2022-05-27 17:52:33 +0300

    code review

--- app/src/main/java/com/example/wowcart/ui/MainActivity.kt
+++ app/src/main/java/com/example/wowcart/ui/MainActivity.kt
@@ -6,16 +6,15 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.wowcart.R
 import com.example.wowcart.databinding.ActivityMainBinding
 
+//TODO: add .idea recommended files to gitignore. Check https://stackoverflow.com/questions/16736856/what-should-be-in-my-gitignore-for-an-android-studio-project
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    //TODO: do not put new line before {, it is java coding style
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        //TODO: viewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setContentView(R.layout.product_recyclerview_item)
     }
 }
--- app/src/main/java/com/example/wowcart/ui/Product.kt
+++ app/src/main/java/com/example/wowcart/ui/Product.kt
@@ -1,5 +1,6 @@
 package com.example.wowcart.ui
 
+//TODO: press format code alt + cmd + L to format code properly
 data class Product(
 val image: String,
 val title: String,
